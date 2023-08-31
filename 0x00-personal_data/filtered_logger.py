d84026c3153a502a8d82a9c81d1d2526460ae088
Jenaide <jenaidesibolie@gmail.com>
2023-08-31 19:17:17 +0000

    added task 0 1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Created by Jenaide Sibolie
+"""
+import os
+import re
+from typing import List
+import logging
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    a fuction that returns the log message abfuscated
+    """
+    extract, replace = (patterns['extract'], patterns['replace'])
+    return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
