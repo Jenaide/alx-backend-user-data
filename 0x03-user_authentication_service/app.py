376a10f069aa3978311f4cb28270b6d3207ef06f
Jenaide <jenaidesibolie@gmail.com>
2023-09-14 19:59:41 +0000

    completed all tasks

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,5 +93,54 @@ def profile() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    flask route to get the password that user reset
+    Returns:
+        the user password
+    """
+    try:
+        email = request.form.get("email")
+        user = AUTH.get_user_by_email(email)
+
+        if user:
+            resetToken = AUTH.get_reset_password_token(email)
+            response_data = {
+                    "email": email,
+                    "reset_token": resetToken
+            }
+            return jsonify(response_data), 200
+        else:
+            abort(403)
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """
+    flask route to update user password
+
+    Return:
+        the user's updated password
+    """
+    try:
+        email = request.form.get("email")
+        resetToken = request.form.get("reset_token")
+        new_pwd = request.form.get("new_password")
+
+        AUTH.update_password(resetToken, new_pwd)
+
+        response_data = {
+                "email": email,
+                "message": "Password updated"
+        }
+        return jsonify(response_data), 200
+    except ValueError:
+        abort(403)
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -96,3 +96,36 @@ class Auth:
             return None
         self._db.update_user(user_id, session_id=None)
 
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        a method that generates a reset token password for the user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+            if user:
+                resetToken = _generate_uuid()
+                self._db.update_user(user.id, resetToken=resetToken)
+                return resetToken
+            else:
+                raise ValueError()
+        except NoResultFound:
+            return None
+
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        a method to update a user's password if the user resets its token
+        """
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if user:
+                hashed_pwd = self._hash_password(password)
+                self._db.update_user(user.id, hashed_pwd=hashed_pwd, reset_token=None)
+            else:
+                raise ValueError()
+        except NoResultFound:
+            return None
+
