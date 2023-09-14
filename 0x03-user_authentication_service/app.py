72842616efa66b7b14e90b426162da486b9707af
Jenaide <jenaidesibolie@gmail.com>
2023-09-14 18:46:39 +0000

    completed sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,5 +35,23 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    flask session route with a POST method
+    Return:
+        the user login
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response_data = {"email": email, "message": "logged in"}
+        response = make_response(jsonify(response_data), 200)
+        return response
+    else:
+        abort(401)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
