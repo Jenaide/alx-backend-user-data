ea5cd22fe16732adddbb662b2de9f6f3799d2330
Jenaide <jenaidesibolie@gmail.com>
2023-09-14 18:34:11 +0000

    added create session for user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,19 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def create_session(self, email: str) -> str:
+        """
+        a method that creates a new session for the user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+            if user is None:
+                return None
+            else:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+        except NoResultFound:
+            return None
